661ccc80d022926014b25dc061b201e96bfd187e
Helena Wang <swan689@uwo.ca>
2021-01-05 00:46:51 -0500

    Try1

--- GuessTheSound.js
+++ GuessTheSound.js
@@ -1137,7 +1137,7 @@ function trialsLoopBegin(trialsLoopScheduler) {
   // set up handler to look after randomisation of conditions etc
   trials = new TrialHandler({
     psychoJS: psychoJS,
-    nReps: 1, method: TrialHandler.Method.RANDOM,
+    nReps: 1, method: TrialHandler.Method.SEQUENTIAL,
     extraInfo: expInfo, originPath: undefined,
     trialList: 'GuessSounds_conditions.xlsx',
     seed: undefined, name: 'trials'
